2765c8407dbf782a4d1466f66c1061d3b89b8d66
Olivier <omalahel@gmail.com>
2023-11-19 13:05:33 +0100

    modify doc .env

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ const userSchema = mongoose.Schema({
     entreprises : [{type:mongoose.Schema.ObjectId, ref:'entreprises'}]
 })
 
+
 const User = mongoose.model('user', userSchema)
 
 module.exports = User
